48ca48f1c3b3f9a2d924dd3162b6bcc0b889077e
Brian Yu <brian@byu.io>
2018-09-27 13:50:22 -0400

    Update models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, RegexValidator
 from django.contrib.auth.models import User as DjangoUser
-from django.contrib.localflavor.us.models import USZipCodeField
 
 class User(models.Model):
     user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
-    zip_code = USZipCodeField()
+    zip_code = models.CharField(
+        max_length = 10,
+        validators=[RegexValidator(r'^\d{5}(?:[-\s]\d{4})?$')]
+    )
 
 class Item(models.Model):
     EXCELLENT = 'E'
@@ -25,15 +27,18 @@ class Item(models.Model):
         choices=CONDITION_CHOICES,
         default=GOOD,)
     description = models.TextField()
-    price_per_day = models.DecimalField(decimal_places=2)
+    price_per_day = models.DecimalField(max_digits=10, decimal_places=2)
     max_borrow_days = models.IntegerField(
         validators = [MinValueValidator(1)]
     )
+    currently_borrowed = models.BooleanField()
 
 
 class Borrow(models.Model):
-    lender = models.ForeignKey(User, on_delete=models.CASCADE)
-    borrower = models.ForeignKey(User, on_delete=models.CASCADE)
+    lender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lent_items")
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowed_items")
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     borrow_date = models.DateTimeField('date borrowed')
-    borrow_days = models.DurationField()
+    borrow_days = models.IntegerField(
+        validators = [MinValueValidator(1)]
+    )
