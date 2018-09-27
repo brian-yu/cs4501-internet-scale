44449ebcb2f419b50a87f3f02274e39a0dd4ecfa
Brian Yu <brian@byu.io>
2018-09-27 13:29:30 -0400

    Add condition choices

--- app/models.py
+++ app/models.py
@@ -5,15 +5,30 @@ class User(models.Model):
     user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
 
 class Item(models.Model):
-	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	name = models.CharField(max_length=100)
-	condition = models.CharField(max_length=20)
-	description = models.TextField()
-	price_per_day = models.DecimalField(decimal_places=2)
+    EXCELLENT = 'E'
+    GOOD = 'G'
+    OKAY = 'O'
+    BAD = 'B'
+    CONDITION_CHOICES = (
+        (EXCELLENT, 'E'),
+        (GOOD, 'G'),
+        (OKAY, 'O'),
+        (BAD, 'B'),
+    )
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    condition = models.CharField(
+        max_length=1,
+        choices=CONDITION_CHOICES,
+        default=GOOD,)
+    description = models.TextField()
+    price_per_day = models.DecimalField(decimal_places=2)
+    max_borrow_duration = models.DurationField()
 
 class Borrow(models.Model):
-	lender = models.ForeignKey(User, on_delete=models.CASCADE)
-	borrower = models.ForeignKey(User, on_delete=models.CASCADE)
-	item = models.ForeignKey(Item, on_delete=models.CASCADE)
-	borrow_date = models.DateTimeField('date borrowed')
-	borrow_duration = models.DurationField()
+    lender = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField('date borrowed')
+    borrow_duration = models.DurationField()
