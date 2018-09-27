dec80975be077d8678b2797959e763d400f7a38f
Brian Yu <brian@byu.io>
2018-09-27 13:23:47 -0400

    First iteration of models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
-# Create your models here.
+class User(models.Model):
+    user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+
+class Item(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=100)
+	condition = models.CharField(max_length=20)
+	description = models.TextField()
+	price_per_day = models.DecimalField(decimal_places=2)
+
+class Borrow(models.Model):
+	lender = models.ForeignKey(User, on_delete=models.CASCADE)
+	borrower = models.ForeignKey(User, on_delete=models.CASCADE)
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	borrow_date = models.DateTimeField('date borrowed')
+	borrow_duration = models.DurationField()
