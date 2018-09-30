263bf309212ba37c10b1b138eebed3536521c1e0
Brian Yu <brian@byu.io>
2018-09-29 23:36:00 -0400

    Use custom docker image

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('users/<int:id>/', views.user),
+    path('items/<int:id>/', views.item),
+    path('borrows/<int:id>/', views.borrow),
+    path('reviews/<int:id>/', views.review),
 ]
--- app/views.py
+++ app/views.py
@@ -1,23 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from .models import User
+from .models import User, Review, Borrow, Item
 from django.forms.models import model_to_dict
-import json
+import simplejson as json
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the app index.")
 
+
+def get(request, model, id):
+    try:
+        obj = model.objects.get(pk=id)
+        obj_dict = model_to_dict( obj )
+        result = json.dumps({'result': obj_dict, 'ok': True})
+        return HttpResponse(result, content_type='application/json')
+    except User.DoesNotExist:
+        result = json.dumps({'error': 'object not found', 'ok': False})
+        return HttpResponse(result, content_type='application/json')
+
 def user(request, id):
     if request.method == "GET":
-        try:
-            user = User.objects.get(pk=id)
-            user_dict = model_to_dict( user )
-            result = json.dumps({'result': user_dict, 'ok': True})
-            return HttpResponse(result, content_type='application/json')
-        except User.DoesNotExist:
-            result = json.dumps({'error': 'user not found', 'ok': False})
-            return HttpResponse(result, content_type='application/json')
+        return get(request, User, id)
+        
+    elif request.method == "POST":
+        return HttpResponse("POST user with id {}".format(id))
+
+def item(request, id):
+    if request.method == "GET":
+        return get(request, Item, id)
+        
+    elif request.method == "POST":
+        return HttpResponse("POST user with id {}".format(id))
+
+def review(request, id):
+    if request.method == "GET":
+        return get(request, Review, id)
+        
+    elif request.method == "POST":
+        return HttpResponse("POST user with id {}".format(id))
+
+def borrow(request, id):
+    if request.method == "GET":
+        return get(request, Borrow, id)
         
     elif request.method == "POST":
         return HttpResponse("POST user with id {}".format(id))
