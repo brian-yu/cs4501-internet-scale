147b919c01cac6b1c127576523b0b9c55eb40029
Brian Yu <brian@byu.io>
2018-10-02 21:14:18 -0400

    remove trailing slash

--- app/urls.py
+++ app/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
     path('reviews/create', views.create_review),
 
     # GET and UPDATE requests
-    path('users/<int:id>/', views.user),
-    path('items/<int:id>/', views.item),
-    path('borrows/<int:id>/', views.borrow),
-    path('reviews/<int:id>/', views.review),
+    path('users/<int:id>', views.user),
+    path('items/<int:id>', views.item),
+    path('borrows/<int:id>', views.borrow),
+    path('reviews/<int:id>', views.review),
 
     # DELETE requests
     path('users/<int:id>/delete', views.delete_user),
