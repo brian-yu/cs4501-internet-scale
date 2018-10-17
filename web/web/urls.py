dd6005d81a0ad485ea6261079d9a71134338903b
Brian Yu <brian@byu.io>
2018-10-16 22:16:32 -0400

    Static files

--- web/web/urls.py
+++ web/web/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-	path('api/v1/', include('web_frontend.urls')),
+	path('', include('web_frontend.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
