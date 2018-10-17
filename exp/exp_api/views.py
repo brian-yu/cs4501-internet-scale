9cc032c1a919b81b58038217164f1ef307780f3b
Brian Yu <brian@byu.io>
2018-10-17 14:53:42 -0400

    Update ALLOWED_HOSTS

--- exp/exp_api/views.py
+++ exp/exp_api/views.py
@@ -13,12 +13,12 @@ def users(req):
 
 def user_detail(req, id):
 	url = 'http://models-api:8000/api/v1/users/{}/'.format(id)
-	url = 'http://models-api:8000/api/v1/'
-	# return HttpResponse("{}".format(url))
 
 	# req = urllib.request.Request(url)
 
 	resp_json = urllib.request.urlopen(url).read().decode('utf-8')
 	resp = json.loads(resp_json)
+
+	# r = requests.get(url)
 
 	return HttpResponse("{}".format(resp_json))
