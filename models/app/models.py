099097c275b9332c25f0b99763fa3a4d27e22c93
Brian Yu <brian@byu.io>
2018-10-28 21:28:35 -0400

    Import functions

--- models/app/models.py
+++ models/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
+from django.contrib.auth.hashers import make_password, check_password
 
 
 class User(models.Model):
