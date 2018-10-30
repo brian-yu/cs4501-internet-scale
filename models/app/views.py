04ba8f6cfee29c74fa3190930d0b5feec4a6705c
Aaron Gu <ag5ym@virginia.edu>
2018-10-29 23:03:42 -0400

    12 featured items

--- models/app/views.py
+++ models/app/views.py
@@ -317,7 +317,7 @@ def create_review(request):
 
 def featured_items(req):
     res = []
-    for item in list(Item.objects.all().order_by('-id')[:10]):
+    for item in list(Item.objects.all().order_by('-id')[:12]):
         d = model_to_dict(item)
         d['owner'] = model_to_dict(User.objects.get(pk=d['owner']))
         res.append(d)
