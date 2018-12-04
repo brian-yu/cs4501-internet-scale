5864584450c6a5fd5773515ea1b03bfa26baa282
Aaron Gu <ag5ym@virginia.edu>
2018-12-03 22:44:46 -0500

    actual selenium

--- selenium.py
+++ selenium.py
@@ -1,14 +1,14 @@
+import time
 from selenium import webdriver
 # from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 driver = webdriver.Chrome()
-driver.get('http://web:8000')
+driver.get('http://localhost:8000')
 # register
 driver.find_element_by_xpath('/html/body/nav/div[3]/ul/li[3]/a').click()
 driver.find_element_by_xpath('//*[@id="id_first_name"]').send_keys('te')
 driver.find_element_by_xpath('//*[@id="id_last_name"]').send_keys('st')
 driver.find_element_by_xpath('//*[@id="id_email"]').send_keys('test@gmail.com')
 driver.find_element_by_xpath('//*[@id="id_zip_code"]').send_keys('12345')
-driver.find_element_by_xpath('//*[@id="id_email"]').send_keys('test@gmail.com')
 driver.find_element_by_xpath('//*[@id="id_password"]').send_keys('testpassword123')
 driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/form/table/tbody/tr[7]/td[2]/button').click()
 
