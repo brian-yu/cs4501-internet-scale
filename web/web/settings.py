24048bf5061d5f4cbea0acdca4a6a0964f1ff425
Brian Yu <brian@byu.io>
2018-12-04 20:05:52 -0500

    Add nginx

--- web/web/settings.py
+++ web/web/settings.py
@@ -117,5 +117,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'nginx'
 STATIC_ROOT = os.path.join(BASE_DIR, 'web_frontend/static')
