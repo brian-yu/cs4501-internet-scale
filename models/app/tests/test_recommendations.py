9352d634f6059df034b9e6ce72b17221ca3568c3
Brian Yu <brian@byu.io>
2018-12-18 20:48:43 -0500

    Add recommendation tests

--- models/app/tests/test_recommendations.py
+++ models/app/tests/test_recommendations.py
@@ -22,18 +22,38 @@ class TestRecommendations(TestCase):
         # self.assertEquals(response['ok'], True)
         # pass
 
-    def test_get_success(self):
+    def test_get(self):
         response = self.client.get('http://localhost:8001/api/v1/recommendations/9/')
         string = response.content.decode('utf-8')
         response = json.loads(string)['result']
 
-        self.assertEquals(response[0]['id'], 12)
+        self.assertEquals(response[0]['recommended_item'], 12)
 
         response = self.client.get('http://localhost:8001/api/v1/recommendations/12/')
         string = response.content.decode('utf-8')
         response = json.loads(string)['result']
 
-        self.assertEquals(response[0]['id'], 9)
+        self.assertEquals(response[0]['recommended_item'], 9)
+
+    def test_get_empty(self):
+        response = self.client.get('http://localhost:8001/api/v1/recommendations/10/')
+        string = response.content.decode('utf-8')
+        response = json.loads(string)['result']
+
+        self.assertEquals(len(response), 0)
+
+    def test_create(self):
+        post_data = {
+            'item1_id': 10,
+            'item2_id': 13,
+        }
+
+        response = self.client.post('http://models-api:8001/api/v1/recommendations/create/', post_data, format='json')
+        self.assertEquals(response.status_code, 200)
+
+        string = response.content.decode('utf-8')
+        response = json.loads(string)
+        self.assertEquals(response['ok'], True)
 
     #tearDown method is called after each test
     def tearDown(self):
