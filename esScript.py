ca0187dc06f33a844ee78792be9395fce49c95ff
Aaron Gu <ag5ym@virginia.edu>
2018-12-22 20:25:04 -0500

    frontpage description

--- esScript.py
+++ esScript.py
@@ -3,10 +3,23 @@
 from kafka import KafkaConsumer
 from kafka.common import NodeNotReadyError
 from elasticsearch import Elasticsearch
+import urllib.request
+import urllib.parse
 import time, json
 
 time.sleep(10)
 es = Elasticsearch(['es'])
+
+# index everything in database
+url = 'http://models-api:8000/api/v1/all_items/'
+resp_json = urllib.request.urlopen(url).read().decode('utf-8')
+resp = json.loads(resp_json)
+items = resp['result']['items']
+for item in items:
+    es.index(index='items_index', doc_type='item', id=item['id'], body=item)
+    es.indices.refresh(index="items_index")
+    time.sleep(1)
+
 while True:
     try:
         consumer = KafkaConsumer('new-items-topic', group_id='items-indexer', bootstrap_servers=['kafka:9092'])
